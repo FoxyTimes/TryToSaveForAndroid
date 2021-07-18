bb285d725d144f7e695a4b7cccfff6c1dc08e74d
FoxyTimes <84171816+FoxyTimes@users.noreply.github.com>
2021-07-18 20:03:58 +0700

    Add files via upload

--- main-kivy.py
+++ main-kivy.py
@@ -13,19 +13,15 @@ class Img(Image):
     pass
 
 
-class Text(Label):
+class Anch(AnchorLayout):
     pass
 
 class MainApp(App):
     def build(self):
         anchor_main = AnchorLayout(anchor_x='center', anchor_y='center')
-        i = Img()
-        anchor_main.add_widget(i)
         b = Container()
-        anchor_for_text = AnchorLayout(anchor_x='center', anchor_y='top')
-        anchor_for_text.add_widget(Text())
-        anchor_main.add_widget(anchor_for_text)
         anchor_main.add_widget(b)
+        anchor_main.add_widget(Anch())
         return anchor_main
 
 if __name__ == '__main__':
