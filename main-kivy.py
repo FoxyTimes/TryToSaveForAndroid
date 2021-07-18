097254a37b7ed8c2b63da67ce72ffc921040e05d
FoxyTimes <84171816+FoxyTimes@users.noreply.github.com>
2021-07-18 21:32:11 +0700

    Add files via upload

--- main-kivy.py
+++ main-kivy.py
@@ -5,24 +5,21 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from functools import partial
 
 class Container(FloatLayout):
-    pass
-
-class Img(Image):
-    pass
-
-
-class Anch(AnchorLayout):
-    pass
+    def remove_start_game_screen(self):
+        self.ids.anch1.remove_widget(self.ids.bt1)
+        self.ids.anch1.remove_widget(self.ids.anch2)
 
 class MainApp(App):
     def build(self):
         anchor_main = AnchorLayout(anchor_x='center', anchor_y='center')
-        b = Container()
-        anchor_main.add_widget(b)
-        anchor_main.add_widget(Anch())
+        self.b = Container()
+        anchor_main.add_widget(self.b)
         return anchor_main
 
 if __name__ == '__main__':
     MainApp().run()
+
